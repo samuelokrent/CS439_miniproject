6f3232b7f5d46593aa57a0a1b18e37983b5e6d70
Sam Okrent <samuelokrent@gmail.com>
2017-12-09 19:25:35 -0600

    play and record scripts

--- play.py
+++ play.py
@@ -2,39 +2,28 @@ import numpy as np
 import pyaudio
 import wave
 import sys
-import time
-import matplotlib.pyplot as plt
+from time import sleep
 
 RATE = 44100
-DURATION = .0005
+DURATION = 0.0005
 
 def sound_array(frequency):
     t = np.linspace(0, DURATION, num = RATE * DURATION, endpoint = False)
     return np.cos(2 * np.pi * frequency * t)
-
 
 # arr is np array of floats between -1 and 1
 def to_bytes(arr):
     shifted = np.array(arr * (2**15 - 1), dtype=np.int16)
     return shifted.tobytes()
 
-
 def create_signal():
     s = []
-    for i in range(1, 6):
-        s = np.append(sound_array(5500 + 1000*i), s)
+    for i in range(5):
+        s = np.append(sound_array(6000 + 1000*i), s)
     return s
 
-
-def main():
+def play():
     p = pyaudio.PyAudio()
-
-
-
-
-    time.sleep(3)
-
-
 
     stream = p.open(format=pyaudio.paInt16,
                     channels=1,
@@ -42,17 +31,16 @@ def main():
                     output=True)
 
     s = create_signal()
-
     stream.write(to_bytes(s))
-    #time.sleep(1)
-    #stream.write(to_bytes(s))
-
     #stream.write(to_bytes(sound_array(6000)))
+
     stream.stop_stream()
     stream.close()
-
     p.terminate()
 
+def main():
+  sleep(1)
+  play()
 
 if __name__ == '__main__':
     main()
--- record.py
+++ record.py
@@ -1,38 +1,72 @@
+import numpy as np
+import os
+from play import create_signal
 import pyaudio
 import wave
+import subprocess
+import sys
+import time
+import matplotlib.pyplot as plt
 
 CHUNK = 1024
 FORMAT = pyaudio.paInt16
-CHANNELS = 2
+CHANNELS = 1
 RATE = 44100
-RECORD_SECONDS = 5
-WAVE_OUTPUT_FILENAME = "output.wav"
+RECORD_SECONDS = 3
 
-p = pyaudio.PyAudio()
+def shift(data):
+    data_array = np.frombuffer(data, dtype='<i2')
+    return np.array(data_array, dtype=np.float64) / ((2**15) - 1.0)
 
-stream = p.open(format=FORMAT,
+def record():
+  p = pyaudio.PyAudio()
+
+  stream = p.open(format=FORMAT,
                 channels=CHANNELS,
                 rate=RATE,
                 input=True,
                 frames_per_buffer=CHUNK)
 
-print("* recording")
+  data = np.array([])
+  while len(data) < (RATE * RECORD_SECONDS):
+    data = np.append(data, shift(stream.read(CHUNK)))
+  stream.stop_stream()
+  stream.close()
+  p.terminate()
+  return data[10000:]
 
-frames = []
+def zoom(data):
+  idx = np.argmax(data)
+  buffer = int(RATE * 0.010)
+  return data[idx - buffer: idx + buffer]
 
-for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
-    data = stream.read(CHUNK)
-    frames.append(data)
+def corr(data):
+  expected_signal = create_signal()
+  corr = np.correlate(data, expected_signal)
+  return corr
+ 
+def main():
+  subprocess.call("python play.py &", shell=True)
+  data = record()
+  plt.plot(data)
+  plt.show()
 
-print("* done recording")
+  data = zoom(data)
+  plt.plot(data)
+  plt.show()
 
-stream.stop_stream()
-stream.close()
-p.terminate()
+  c = corr(data)
+  m = np.argmax(c)
+  plt.plot(c)
+  c_copy = c.copy()
+  l = len(create_signal())
+  c_copy[m - round(l/2):m+round(l/2)] = 0.0
+  m2 = np.argmax(c_copy)
+  plt.plot(c_copy)
+  plt.show()
+  
+  print(m, m2)
+  print(((m2 - m) / (RATE * 1.0)) * 343 / 2.0)
 
-wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
-wf.setnchannels(CHANNELS)
-wf.setsampwidth(p.get_sample_size(FORMAT))
-wf.setframerate(RATE)
-wf.writeframes(b''.join(frames))
-wf.close()
+if __name__ == '__main__':
+    main()
